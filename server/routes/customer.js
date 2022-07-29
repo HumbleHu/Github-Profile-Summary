67d3b5dfed7cad7f9dc9e5f9bc91f75335a2a0e7
Shixiang Yang <adam_yang2021@163.com>
2022-07-28 23:50:20 -0700

    add basic query using axios

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+// guser => github_user
 
+const hostname = "api.github.com";
+router.get("/guser/:guser", (req, res) => {
+	axios
+		.get("https://api.github.com/users/Adamyang1")
+		.then((result) => {
+			res.send(result.data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 module.exports = router;
