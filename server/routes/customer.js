2924d2d4f20d7009730c49cd1c63e3e2bd45d575
Shixiang Yang <adam_yang2021@163.com>
2022-07-30 20:26:35 -0700

    get stars for each repo & sort successfully

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -17,6 +17,28 @@ router.get("/guser/:guser/general", (req, res) => {
 });
 
 router.get("/guser/:guser/repos", (req, res) => {
-	axios.get(`${hostname}/`);
+	const { guser } = req.params;
+	var repos = [];
+	var res = [];
+	axios
+		.get(`${hostname}/users/${guser}/repos`)
+		.then((result) => {
+			repos = result.data;
+			repos.forEach((repo) => {
+				var repo_obj = new Object();
+				repo_obj.name = repo.name;
+				repo_obj.stars = repo.stargazers_count;
+				res.push(repo_obj);
+			});
+			// console.log(res);
+			res.sort(function (a, b) {
+				return a.stars - b.stars;
+			});
+			console.log(res);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
+
 module.exports = router;
